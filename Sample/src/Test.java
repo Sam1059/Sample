1786b4f1628713bbc831a77e459ba19e9f1929b0
Sam1059 <samuingole10@gmail.com>
2025-03-17 16:25:02 +0530

    Update Test.java

--- Sample/src/Test.java
+++ Sample/src/Test.java
@@ -4,6 +4,7 @@ public void m1()
 {
 System.out.println("Hello World");
 System.out.println("Good Afternoon");
+System.out.printlnz("Hii...");
 }
 public static void main(String[] args) {
 	Test t= new Test();

76591373416cfe4739a58f41fdad45b4bcc36d80
Admin <Admin@SAM-10.lan>
2025-03-17 16:23:55 +0530

    new message

--- Sample/src/Test.java
+++ Sample/src/Test.java
@@ -2,7 +2,8 @@
 public class Test {
 public void m1()
 {
-System.out.println("Hello World");	
+System.out.println("Hello World");
+System.out.println("Good Afternoon");
 }
 public static void main(String[] args) {
 	Test t= new Test();
